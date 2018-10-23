1c813a4a075902c3727b52a7f9766a1b1091961a
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-10-23 01:26:06 +0100

    middle_pos e cleanup de código

--- nome.py
+++ nome.py
@@ -12,33 +12,21 @@ def is_board(board):
                 return False
     return True
 
+def get_pos(board, pos):
+    return board[pos_l(pos)][pos_c(pos)]
+
+#def change_content(board, pos, content):
+
+
 def get_content(board, pos):
     if pos_l(pos) < 0 or pos_l(pos) > len(board) - 1 or pos_c(pos) < 0 or pos_c(pos) > len(board[pos_l(pos)]) - 1:
         raise ValueError("out of bounds")
-    return board[pos_l(pos)][pos_c(pos)]
+    return get_pos(board, pos)
 
-def check_up(board, pos):
-    try:
-        if is_peg(get_content(up_pos(pos))) and is_empty(get_content(up_pos(up_pos(pos)))):
-            return True
-        else:
-            return False
-    except ValueError:
-        return False
-
-def check_down(board, pos):
-    try:
-        if is_peg(get_content(down_pos(pos))) and is_empty(get_content(down_pos(down_pos(pos)))):
-            return True
-        else:
-            return False
-    except ValueError:
-        return False
-
-def check(board, pos, func):
+def check_move(board, pos, func):
     try:
         if is_peg(get_content(board, func(pos))) and is_empty(get_content(board, func(func(pos)))):
-            return [pos, func(func(pos))]
+            return make_move(pos, func(func(pos)))
         else:
             return []
     except ValueError:
@@ -49,7 +37,7 @@ def check_pos_moves(board, pos):
     if not is_peg(get_content(board, pos)): #falta um try aqui
         raise ValueError("not a peg")
     for f in [up_pos, down_pos, left_pos, right_pos]:
-        m = check(board, pos, f)
+        m = check_move(board, pos, f)
         if m != []:
             moves.append(m)
     return moves
@@ -117,3 +105,15 @@ def move_initial (move):
 
 def move_final (move):
     return move[1]
+
+def middle_pos(move):
+    if pos_l(move_initial(move)) == pos_l(move_final(move)):
+        if pos_c(move_initial(move)) < pos_c(move_final(move)):
+            return right_pos(move_initial(move))
+        else:
+            return left_pos(move_initial(move))
+    else:
+        if pos_c(move_initial(move)) < pos_c(move_final(move)):
+            return up_pos(move_initial(move))
+        else:
+            return down_pos(move_initial(move))
